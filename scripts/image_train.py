2028ce00b1f55979311ef469913831fdd8825393
Ludvig Skare <ludvig@skare.se>
2024-12-05 18:34:08 +0100

    add tmp dir path to repo instead of os tmp folder

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -20,7 +20,7 @@ def main():
     args = create_argparser().parse_args()
 
     dist_util.setup_dist()
-    logger.configure()
+    logger.configure(dir="./checkpoints")
 
     print("Using: ", dist_util.dev())
 
